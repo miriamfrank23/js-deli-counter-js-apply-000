262953f5a208d5f373742e256ee10109a559d457
Miriam Frank <miriamfrank23@gmail.com>
2018-05-28 17:26:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeli = [];
+
+function takeANumber (katzDeliLine, name) {
+
+}
+
+function nowServing (katzDeliLine) {
+if (katzDeliLine.length > 0) {
+  return katzdeliLine.shift();
+} else {
+  return "There is nobody waiting to be served!";
+ }
+}
+
+function currentLine (name) {
