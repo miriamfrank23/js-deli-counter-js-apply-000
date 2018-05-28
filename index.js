9af1d93782c58d4245e583c6b6edc093c0bea9cf
Miriam Frank <miriamfrank23@gmail.com>
2018-05-28 22:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function fixArray (line) {
 };
 
 function currentLine (line) {
-  if (line.length > 0) {
+  if (line.length() > 0) {
      return fixArray();
   } else {
     return 'The line is currently empty.';
